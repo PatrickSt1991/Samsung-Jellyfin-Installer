34e24c19c3d2cbd409be2586653052a22df9c4d8
PatrickSt1991 <patrick.stel@kpnmail.nl>
2025-08-07 21:32:56 +0200

    version bump

--- Settings.Designer.cs
+++ Settings.Designer.cs
@@ -97,7 +97,7 @@ namespace Samsung_Jellyfin_Installer {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v1.6.7-beta")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v1.6.8-beta")]
         public string AppVersion {
             get {
                 return ((string)(this["AppVersion"]));
