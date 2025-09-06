4daba7fe62129f19c11ccbe5aeac2f6e34f7a118
PatrickSt1991 <patrick.stel@kpnmail.nl>
2025-09-06 14:12:36 +0200

    remove beta tag

--- Settings.Designer.cs
+++ Settings.Designer.cs
@@ -97,7 +97,7 @@ namespace Samsung_Jellyfin_Installer {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v1.7.5-beta")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v1.7.5")]
         public string AppVersion {
             get {
                 return ((string)(this["AppVersion"]));
