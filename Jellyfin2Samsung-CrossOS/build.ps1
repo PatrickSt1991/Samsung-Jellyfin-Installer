331d1552fb8141d2881e1b5db3da504b4a758fe4
PatrickSt1991 <patrick.stel@kpnmail.nl>
2025-09-27 14:45:07 +0200

    added arm64

--- Jellyfin2Samsung-CrossOS/build.ps1
+++ Jellyfin2Samsung-CrossOS/build.ps1
@@ -14,19 +14,34 @@ if (Test-Path $OutputDir) {
 Write-Host "Publishing for Windows..." -ForegroundColor Green
 dotnet publish -c $Configuration -r win-x64 --self-contained -o "$OutputDir/win-x64"
 
-Write-Host "Publishing for macOS..." -ForegroundColor Green
+Write-Host "Publishing for macOS x64..." -ForegroundColor Green
 dotnet publish -c $Configuration -r osx-x64 --self-contained -o "$OutputDir/osx-x64"
+
+Write-Host "Publishing for macOS arm64..." -ForegroundColor Green
+dotnet publish -c $Configuration -r osx-arm64 --self-contained -o "$OutputDir/osx-arm64"
 
 Write-Host "Publishing for Linux..." -ForegroundColor Green
 dotnet publish -c $Configuration -r linux-x64 --self-contained -o "$OutputDir/linux-x64"
 
-# Create macOS app bundle
+# Create macOS osx64 app bundle
 Write-Host "Creating macOS app bundle..." -ForegroundColor Green
 $MacOSAppPath = "$OutputDir/osx-x64/$ProjectName.app"
 New-Item -ItemType Directory -Path "$MacOSAppPath/Contents/MacOS" -Force
 New-Item -ItemType Directory -Path "$MacOSAppPath/Contents/Resources" -Force
 
 Move-Item "$OutputDir/osx-x64/$ProjectName" "$MacOSAppPath/Contents/MacOS/"
+Copy-Item "Info.plist" "$MacOSAppPath/Contents/"
+if (Test-Path "Assets/jelly2sams.icns") {
+    Copy-Item "Assets/jelly2sams.icns" "$MacOSAppPath/Contents/Resources/"
+}
+
+# Create macOS osx-arm64 app bundle
+Write-Host "Creating macOS osx-arm64 app bundle..." -ForegroundColor Green
+$MacOSAppPath = "$OutputDir/osx-arm64/$ProjectName.app"
+New-Item -ItemType Directory -Path "$MacOSAppPath/Contents/MacOS" -Force
+New-Item -ItemType Directory -Path "$MacOSAppPath/Contents/Resources" -Force
+
+Move-Item "$OutputDir/osx-arm64/$ProjectName" "$MacOSAppPath/Contents/MacOS/"
 Copy-Item "Info.plist" "$MacOSAppPath/Contents/"
 if (Test-Path "Assets/jelly2sams.icns") {
     Copy-Item "Assets/jelly2sams.icns" "$MacOSAppPath/Contents/Resources/"
